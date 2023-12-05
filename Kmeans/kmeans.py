21210cb39526ec96c8f1d88b042e188cfd1231bc
PaivarintaJohannes <josku698@gmail.com>
2023-12-05 15:51:06 +0200

    hienosäätöä

--- Kmeans/kmeans.py
+++ Kmeans/kmeans.py
@@ -3,24 +3,28 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 ## Määritellään data yms
-data = np.genfromtxt('data.csv',delimiter=',')
-
-coordinates = np.zeros((250,3))
-
-direction_ids = data[:,0]
+data = np.genfromtxt('data1.csv',delimiter=',')
 coordinates = data[:,1:]
-
+direction_ids = data[:,0]
 numberOfRows = coordinates.shape[0]
 numberOfRows = int(numberOfRows)
 max_values = np.max(coordinates, axis=0)
-centerPointCumulativeSum = np.zeros((5,3))
-counts = np.zeros((1,5))
-new_centers = np.zeros((5,3))
-amountOfCenters = 5
-distances = np.zeros((1, 5))
-k_centers = coordinates[np.random.choice(numberOfRows, size=amountOfCenters, replace=False)]
+centerPointCumulativeSum = np.zeros((6,3))
+counts = np.zeros((1,6))
+new_centers = np.zeros((6,3))
+amountOfCenters = 6
 
-
+##randompointtien määrittely
+def kMeansPlusPlusInit(data, k):
+    centers = np.zeros((k, data.shape[1]))
+    centers[0] = data[np.random.choice(data.shape[0])]
+    
+    for i in range(1, k):
+        distances = np.array([min(np.linalg.norm(x - center) for center in centers[:i]) for x in data])
+        probabilities = distances**2 / np.sum(distances**2)
+        centers[i] = data[np.random.choice(data.shape[0], p=probabilities)]
+    
+    return centers
 ##debuggaamiseen
 def debugPrints():
     print("numberOfRows = ", numberOfRows)
@@ -28,14 +32,14 @@ def debugPrints():
     print("k_centers = \n", k_centers)
     print("centerPointCumulativeSum = \n",centerPointCumulativeSum)
     print("new_centers = \n",new_centers)
-
+k_centers = kMeansPlusPlusInit(coordinates, amountOfCenters)
 def plottaus():
     fig = plt.figure()
     colors = plt.cm.rainbow(direction_ids / direction_ids.max())
     ax = fig.add_subplot(111, projection='3d')
-    for i in range(5):
-        ax.scatter(new_centers[i,0],new_centers[i,1],new_centers[i,2], c='black', marker='X', label='Calculated Centers' if i == 0 else '')
-    for i in range(5):
+    for i in range(6):
+        ax.scatter(new_centers[i,0],new_centers[i,1],new_centers[i,2], c='black', marker='x', label='Calculated Centers' if i == 0 else '')
+    for i in range(6):
         ax.scatter(k_centers[i,0],k_centers[i,1],k_centers[i,2], c='b', marker='o',label='Random Points' if i == 0 else '')
     for direction_id in np.unique(direction_ids):
         indices = direction_ids == direction_id
@@ -55,34 +59,48 @@ def calculateNewCenters():
         else:          
             new_centers[i, :] = k_centers[i, :]
 
-
+## Määritellään suunnat keskipisteille
+def suunnat():
+    direction_ids_for_clusters = np.zeros(amountOfCenters, dtype=int)
+    for cluster_index in range(amountOfCenters):
+        indices_in_cluster = np.where(assigned_clusters == cluster_index)[0]
+        if len(indices_in_cluster) > 0:
+            direction_id_for_cluster = direction_ids[indices_in_cluster].astype(int)
+            most_frequent_direction_id = np.argmax(np.bincount(direction_id_for_cluster))
+            direction_ids_for_clusters[cluster_index] = most_frequent_direction_id
+    for cluster_index, direction_id in enumerate(direction_ids_for_clusters):
+        center = new_centers[cluster_index, :]
+        print(f"Suunta{direction_id}_center: {center}")
+## Tarkistetaan että countit on ok
 def checkZeros():
-    if np.any(counts == 0): 
+    if np.any(counts == 0):         
         zero_index = np.where(counts == 0)[0][0]
         k_centers[zero_index, :] = np.random.rand(3) * max_values 
-        kMeans()
+        kMeans()    
     else:
        calculateNewCenters()
-
+## ite pää-funktio
 def kMeans():
+    
     for i in range(numberOfRows):
         datapoint = coordinates[i,:]
-        distances = np.zeros((1, 5))
-        for j in range(5):
+        distances = np.zeros((1, 6))
+        for j in range(6):
             distances[0, j] = np.linalg.norm(datapoint - k_centers[j,:])     
         winner = np.argmin(distances)
         centerPointCumulativeSum[winner,:] += datapoint
         counts[0,winner] += 1
+    
     checkZeros()
       
 
 for i in range(10):
-    kMeans()
-    
+   kMeans()
+assigned_clusters = np.argmin(np.linalg.norm(coordinates - new_centers[:, np.newaxis, :], axis=2), axis=0)
 
 plottaus()
-print(new_centers)
+##print(counts)
+suunnat()
 
 
-  
 
